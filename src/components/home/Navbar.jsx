8b2d6ee9017c1b36cbc8346df7f59096dfd48d32
nomso68 <oganchinomso2014@gmail.com>
2025-01-01 15:36:04 +0100

    sign-up and routes

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import Logo from '../../assets/lifelogo.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -15,7 +16,9 @@ const Navbar = () => {
         <li>Consult a Doctor</li>
         <li><a href="#Insights" className="nav-link">Blog</a></li>
         <li><a href="#Footer" className="nav-link">Contact Us</a></li>
-        <a href="#Sign"><button className='btn1'>Sign Up</button></a>
+        <Link to={'/register'}>
+          <button className='btn1'>Sign Up</button>
+        </Link>
         <a href="#"><button className='btn2'>Book Appointment</button></a>
       </ul>
       </div>
